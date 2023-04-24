10baa7904947fc147b8a3204085ff3046ae68785
theRoboxx <sosto100@hhu.de>
2023-04-24 02:18:02 +0200

    added mappings for buffers navigation

--- mappings.lua
+++ mappings.lua
@@ -11,13 +11,18 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+          require("astronvim.utils.buffer").close(
+            bufnr)
+        end)
       end,
       desc = "Pick to close",
     },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<S-h>"] = { "<cmd>bprev<cr>", desc = "previous buffer" },
+    ["<S-l>"] = { "<cmd>bnext<cr>", desc = "next buffer" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
