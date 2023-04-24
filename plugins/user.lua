d73fd549b2444c82702f9434bb1f450c3a13a9dc
theRoboxx <sosto100@hhu.de>
2023-04-24 22:16:50 +0200

    adjust settings and added TODO highlighting

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,3 +1,4 @@
+-- TODO:  Test
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
@@ -9,4 +10,20 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("todo-comments").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end,
+    event = "User AstroFile",
+    cmd = "TodoQuickFix",
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    },
+  },
 }
